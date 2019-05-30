cca04f3126dd44069369231f947da651c2fe2dba
Alicia Fasciocco <afasciocco@gmail.com>
2019-05-30 00:01:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,15 +20,15 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  fun_things.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  fun_things[1]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  fun_things[0]
 end
 
 def retrieve_last_element_from_array(array)
