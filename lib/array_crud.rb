1dba36b4b6b820505ebd6a415f1d71a38f08fe37
Alicia Fasciocco <afasciocco@gmail.com>
2019-05-30 00:04:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,33 +3,33 @@ def create_an_empty_array
 end
 
 def create_an_array
-  array["reading", "writing", "sleeping", "coding"]
+  array = ["reading", "writing", "sleeping", "coding"]
 end
 
 def add_element_to_end_of_array(array, element)
-  create_an_array << "yoga"
+  array << "yoga"
 end
 
 def add_element_to_start_of_array(array, element)
-  create_an_array.unshift "running"
+  array.unshift "running"
 end
 
 def remove_element_from_end_of_array(array)
-  create_an_array.pop
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  create_an_array.shift
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  create_an_array[1]
+  array[1]
 end
 
 def retrieve_first_element_from_array(array)
-  create_an_array[0]
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  create_an_array[-1]
+  array[-1]
 end
