f09010c9c86ec359f4c52eeea1a791a53f29f60a
Alicia Fasciocco <afasciocco@gmail.com>
2019-05-30 00:01:00 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  fun_things.unshift
+  fun_things.unshift "running"
 end
 
 def remove_element_from_end_of_array(array)
